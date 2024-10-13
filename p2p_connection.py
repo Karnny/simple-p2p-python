ae1548d13f3c5c3b018f9a6600fa3970d9873000
kann1er <sivakait@gmail.com>
2024-10-13 10:27:41 +0700

    clean up

--- p2p_connection.py
+++ p2p_connection.py
@@ -13,6 +13,7 @@ peer_port = int(input("Enter the peer's port: "))
 
 talking_port = 42424
 
+
 def receive_messages():
     print(f"Start listening..")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,25 +22,17 @@ def receive_messages():
     
     while True:
         data, addr = sock.recvfrom(1024)
-        print(f"\nPeer {addr}: {data.decode()}")
+        print(f"Peer {addr}: {data.decode()}\n")
+        print("Enter message to send: ", end=" ")
 
 def main():
     
     # Start the receiving thread
     recv_thread = threading.Thread(target=receive_messages, daemon=True)
     recv_thread.start()
-    
-    print('Start punching hole..')
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Enable address reuse
-    sock.bind(('0.0.0.0', peer_port))
-    sock.sendto(b'0', (peer_ip, talking_port))
-    
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Enable address reuse
-    sock.bind(('0.0.0.0', talking_port))
-    
+
     while True:
         message = input("Enter message to send: ")
         sock.sendto(message.encode(), (peer_ip, peer_port))
